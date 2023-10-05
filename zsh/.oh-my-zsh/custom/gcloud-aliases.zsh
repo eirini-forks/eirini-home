5e65641a84deb9df97546756b23772326a708339
Danail Branekov <danailster@gmail.com>
2023-10-05 08:50:23 +0000

    Ensure gcloud is on the PATH
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- zsh/.oh-my-zsh/custom/gcloud-aliases.zsh
+++ zsh/.oh-my-zsh/custom/gcloud-aliases.zsh
@@ -1,3 +1,6 @@
+source "$HOME/google-cloud-sdk/completion.zsh.inc"
+source "$HOME/google-cloud-sdk/path.zsh.inc"
+
 alias gcloudlogin='gcloud auth login'
 alias gcloudclustereirini='gcloud container clusters get-credentials --zone europe-west1-b --project cff-eirini-peace-pods'
 alias gcloudcluster='gcloud container clusters get-credentials --zone europe-west1-b --project cf-on-k8s-wg'
