52f924add306a79ab44c13622612345613290086
Georgi Sabev <georgethebeatle@gmail.com>
2023-10-26 11:19:42 +0000

    Improve k8s prompt
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- zsh/.oh-my-zsh/custom/k8s-prompt.zsh
+++ zsh/.oh-my-zsh/custom/k8s-prompt.zsh
@@ -1,10 +1,16 @@
-# Show current kubectl cluster and namespace
 precmd_kubectl_context() {
-    local context
     RPROMPT=""
+
+    local kubeconfig context
+    kubeconfig="${KUBECONFIG:-$HOME/.kube/config}"
+    if [[ ! -f "$kubeconfig" ]]; then
+        return
+    fi
+
     if context="$(kubectl config current-context 2>/dev/null)"; then
       context="$(yq ".contexts | map(select(.name == \"$(kubectl config current-context)\"))| .[].context.cluster" ${KUBECONFIG:-$HOME/.kube/config})"
-      RPROMPT="%{$fg[blue]%}[${context}]%{$reset_color%}"
+      RPROMPT="%{$fg[blue]%}⎈${context}⎈%{$reset_color%}"
     fi
 }
+
 add-zsh-hook precmd precmd_kubectl_context
