46ce7effa4bb46c7e285d3975976566bf7c00bba
Georgi Sabev <georgethebeatle@gmail.com>
2023-10-26 08:42:04 +0000

    K8s info in RPROMPT respects KUBECONFIG
    Also remove the get-current-cluster script, since it is no longer used

--- zsh/.oh-my-zsh/custom/k8s-prompt.zsh
+++ zsh/.oh-my-zsh/custom/k8s-prompt.zsh
@@ -3,7 +3,7 @@ precmd_kubectl_context() {
     local context
     RPROMPT=""
     if context="$(kubectl config current-context 2>/dev/null)"; then
-      context="$(yq ".contexts | map(select(.name == \"$(kubectl config current-context)\"))| .[].context.cluster" ~/.kube/config)"
+      context="$(yq ".contexts | map(select(.name == \"$(kubectl config current-context)\"))| .[].context.cluster" ${KUBECONFIG:-$HOME/.kube/config})"
       RPROMPT="%{$fg[blue]%}[${context}]%{$reset_color%}"
     fi
 }
