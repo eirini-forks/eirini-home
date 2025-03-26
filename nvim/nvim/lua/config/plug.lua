85dc7c555e7c55d916083269d5897e510e562a5e
Georgi Sabev <georgethebeatle@gmail.com>
2025-03-26 14:00:59 +0000

    Install copilot in nvim

--- nvim/nvim/lua/config/plug.lua
+++ nvim/nvim/lua/config/plug.lua
@@ -311,4 +311,19 @@ require("lazy").setup({
           require("refactoring").setup()
         end,
     },
+    -- ytt syntax highlighting
+    'github/copilot.vim',
+    -- copilot chat
+    {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    },
 })
