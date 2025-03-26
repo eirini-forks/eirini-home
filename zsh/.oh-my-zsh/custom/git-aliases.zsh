73eb098b4503ac9ca6ad2bc9d033963b94b740d6
Georgi Sabev <georgethebeatle@gmail.com>
2025-03-26 12:47:05 +0000

    Improve rel-log
    Use git -C to avoid altering the working directory of the shell

--- zsh/.oh-my-zsh/custom/git-aliases.zsh
+++ zsh/.oh-my-zsh/custom/git-aliases.zsh
@@ -3,10 +3,7 @@ alias gsu='git submodule update --init --recursive'
 alias gdo='git branch | grep -ve "^*" | xargs --max-args=1 git branch --delete --force'
 
 rel-log() {
-  pushd "$HOME/workspace/korifi" >/dev/null
-  {
-    latest_release="$(git for-each-ref --sort=creatordate --shell --format 'r=%(refname); echo ${r#refs/tags/}' refs/tags | xargs -I{} bash -c {} | tail -1)"
-    git log ${latest_release}..HEAD --pretty=format:'https://github.com/cloudfoundry/korifi/commit/%H %s %C(bold blue)<%al>%Creset' --abbrev-commit | grep -v dependabot | grep -v "Merge pull" | grep -v Korifi-Bot
-  }
-  popd >/dev/null
+  korifi_home="$HOME/workspace/korifi"
+  latest_release="$(git -C $korifi_home for-each-ref --sort=creatordate --shell --format 'r=%(refname); echo ${r#refs/tags/}' refs/tags | xargs -I{} bash -c {} | tail -1)"
+  git -C "$korifi_home" log ${latest_release}..HEAD --pretty=format:'https://github.com/cloudfoundry/korifi/commit/%H %s %C(bold blue)<%al>%Creset' --abbrev-commit | grep -v dependabot | grep -v "Merge pull" | grep -v Korifi-Bot
 }
