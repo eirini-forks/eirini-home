816ca8abf1f57b53e841b854423328a71fad9280
Georgi Sabev <georgethebeatle@gmail.com>
2025-10-15 14:20:03 +0000

    Adapt the gcloudcluster alias to the new gcp zone

--- zsh/.oh-my-zsh/custom/gcloud-aliases.zsh
+++ zsh/.oh-my-zsh/custom/gcloud-aliases.zsh
@@ -2,4 +2,4 @@ source "$HOME/google-cloud-sdk/completion.zsh.inc"
 source "$HOME/google-cloud-sdk/path.zsh.inc"
 
 alias gcloudlogin='gcloud auth login'
-alias gcloudcluster='gcloud container clusters get-credentials --zone europe-west1-b --project cf-on-k8s-wg'
+alias gcloudcluster='gcloud container clusters get-credentials --zone europe-north1-a --project cf-on-k8s-wg'
