18f5b2efa1b742f0af5a663a959e19d2e4487cf9
Danail Branekov <danailster@gmail.com>
2026-02-27 15:14:00 +0000

    Fix rel-log
    `Korifi-Bot` has been renamed to `cloudfoundry-korifi`
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- zsh/.oh-my-zsh/custom/git-aliases.zsh
+++ zsh/.oh-my-zsh/custom/git-aliases.zsh
@@ -5,5 +5,5 @@ alias gdo='git branch | grep -ve "^*" | xargs --max-args=1 git branch --delete -
 rel-log() {
   korifi_home="$HOME/workspace/korifi"
   latest_release="$(git -C $korifi_home for-each-ref --sort=creatordate --shell --format 'r=%(refname); echo ${r#refs/tags/}' refs/tags | xargs -I{} bash -c {} | tail -1)"
-  git -C "$korifi_home" log ${latest_release}..HEAD --pretty=format:'https://github.com/cloudfoundry/korifi/commit/%H %s %C(bold blue)<%al>%Creset' --abbrev-commit | grep -v dependabot | grep -v "Merge pull" | grep -v Korifi-Bot
+  git -C "$korifi_home" log ${latest_release}..HEAD --pretty=format:'https://github.com/cloudfoundry/korifi/commit/%H %s %C(bold blue)<%al>%Creset' --abbrev-commit | grep -v dependabot | grep -v "Merge pull" | grep -v cloudfoundry-korifi
 }
